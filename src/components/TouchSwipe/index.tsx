a59ef2de0f793e67e51fdc9281d218a1f7dde050
Aimho <jjhwhwnsgh@gmail.com>
2022-03-14 21:20:52 +0900

    fix: touch 기능 동작 하지 않아서 수정함

--- src/components/TouchSwipe/index.tsx
+++ src/components/TouchSwipe/index.tsx
@@ -1,5 +1,5 @@
 import { Box } from "@mui/material";
-import { useState } from "react";
+import React, { useState } from "react";
 
 export interface IProps {
   onChangeIndex: (index: number) => void;
@@ -10,26 +10,43 @@ const TouchSwipe: React.FC<IProps> = (props) => {
   const [touchStart, setTouchStart] = useState(0);
   const [touchEnd, setTouchEnd] = useState(0);
 
-  function handleTouchStart(e: React.TouchEvent) {
-    setTouchStart(e.targetTouches[0].clientX);
-  }
+  const onSwipe = (start: number, end: number) => {
+    const distance = start - end;
+    if (Math.abs(distance) < 20) return;
 
-  function handleTouchMove(e: React.TouchEvent) {
-    setTouchEnd(e.targetTouches[0].clientX);
-  }
-
-  function handleTouchEnd() {
-    const changeIndex = touchStart - touchEnd > 150 ? 1 : -1;
+    const changeIndex = distance > 0 ? 1 : -1;
     onChangeIndex(changeIndex);
-  }
+
+    setTouchStart(0);
+    setTouchEnd(0);
+  };
+
+  const onTouchStart = (e: React.TouchEvent) => {
+    setTouchStart(e.targetTouches[0].clientX);
+  };
+  const onTouchMove = (e: React.TouchEvent) => {
+    setTouchEnd(e.targetTouches[0].clientX);
+  };
+  const onTouchEnd = () => {
+    onSwipe(touchStart, touchEnd);
+  };
+
+  const onMouseDown = (e: React.MouseEvent) => {
+    setTouchStart(e.clientX);
+  };
+  const onMouseUp = (e: React.MouseEvent) => {
+    onSwipe(touchStart, e.clientX);
+  };
 
   return (
     <Box
       minHeight={300}
       component="section"
-      onTouchStart={handleTouchStart}
-      onTouchMove={handleTouchMove}
-      onTouchEnd={handleTouchEnd}
+      onTouchStart={onTouchStart}
+      onTouchMove={onTouchMove}
+      onTouchEnd={onTouchEnd}
+      onMouseDown={onMouseDown}
+      onMouseUp={onMouseUp}
     >
       {children}
     </Box>
